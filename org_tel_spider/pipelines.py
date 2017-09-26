bdf8abe1bae7744c0e27b358eda7f3db8ab6d504
Daniel Zhang <zhangdaniel0715@gmail.com>
2017-09-26 20:26:45 +0800

    add json output function into spider's pipeline

--- org_tel_spider/pipelines.py
+++ org_tel_spider/pipelines.py
@@ -4,8 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+import codecs
 
 
 class OrgTelSpiderPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('organization_telnum_utf8.json', mode='wb', encoding='utf-8')
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + '\n'
+        #self.file.write(line.decode('unicode_escape'))
+        self.file.write(line)
         return item
+
+    def close_spider(self, spider):
+        self.file.close()
